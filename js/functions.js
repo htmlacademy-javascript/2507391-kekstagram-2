b427bb14c29deec92c2fb1a349122743aa877850
pollirey <psmirnova9@yandex.ru>
2025-11-20 18:51:13 +0300

    Добавление фото миниатюр

--- js/functions.js
+++ js/functions.js
@@ -14,13 +14,3 @@ export function isPalindrome(str) {
 
   return normaLizedStr === reversedStr;
 }
-
-// Тестирование функций (можно оставить или убрать)
-console.log(checkStringLength('проверяемая строка', 20)); // true
-console.log(checkStringLength('проверяемая строка', 18)); // true
-console.log(checkStringLength('проверяемая строка', 10)); // false
-
-console.log(isPalindrome('топот')); // true
-console.log(isPalindrome('ДовОд')); // true
-console.log(isPalindrome('Кекс')); // false
-console.log(isPalindrome('Лёша на полке клопа нашёл ')); // true
