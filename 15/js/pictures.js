29e3c85caffe1d2c8e55a02b6c2f28ae5db88087
Keks <keks@htmlacademy.ru>
2026-01-20 12:50:43 +0300

    :heavy_check_mark: Сборка #15

---
+++ 15/js/pictures.js
@@ -0,0 +1,23 @@
+import { similarPhotos } from './data.js';
+import { openBigPicture } from './full-size.js';
+
+const picturesContainer = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const picturesListFragment = document.createDocumentFragment();
+
+similarPhotos.forEach((photoData) => {
+  const pictureElement = pictureTemplate.cloneNode(true);
+
+  pictureElement.querySelector('.picture__img').src = photoData.url;
+  pictureElement.querySelector('.picture__img').alt = photoData.description;
+  pictureElement.querySelector('.picture__likes').textContent = photoData.likes;
+  pictureElement.querySelector('.picture__comments').textContent = photoData.comments.length;
+
+  pictureElement.addEventListener('click', () => {
+    openBigPicture(photoData);
+  });
+
+  picturesListFragment.appendChild(pictureElement);
+});
+
+picturesContainer.appendChild(picturesListFragment);
